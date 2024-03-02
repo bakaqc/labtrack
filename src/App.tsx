9571dc82859a689253d45dbe68e415837336c1fc
Minh Vương <hardingadonis@gmail.com>
2024-03-02 22:45:43 +0700

    add tailwind to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-
 const App = () => {
   return (
-    <div>
-      <h1>Hello! This is Assignment of FER202 Spring 2024</h1>
-    </div>
+    <>
+      <h1 className="w-full h-screen text-axl text-blue-700 flex items-center justify-center">
+        Hello! This is Assignment of FER202 Spring 2024
+      </h1>
+    </>
   );
 };
 
