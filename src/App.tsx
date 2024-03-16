81bf7917ec42466e7975bb2fd42db3f420b570b3
Minh Vương <hardingadonis@gmail.com>
2024-03-17 00:13:58 +0700

    batch 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 const App = () => {
 	return (
 		<>
-			<h1 className="w-full h-screen text-axl text-blue-700 flex items-center justify-center">
-				Hello! This is Assignment of FER202 Spring 2024
-			</h1>
+			<h1>Hello! This is Assignment of FER202 Spring 2024</h1>
 		</>
 	);
 };
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,4 @@
-export type ProductInfo = {
+declare type ProductInfo = {
 	id: number;
 	name: string;
 	description: string;
