4dce1c00b419086df8666400d859e28b10a4a6fe
Minh Vương <hardingadonis@gmail.com>
2024-03-21 00:22:02 +0700

    update router config

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,20 +1,77 @@
+/* eslint-disable react-refresh/only-export-components */
+import { Suspense, lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-import LoginPage from '../pages/login/LoginPage';
+import LoadingComponent from '../components/common/LoadingComponent';
 import Error404 from '../pages/error/Error404';
-import ProductListPage from '../pages/products/ProductListPage';
-import AddProductPage from '../pages/products/AddProductPage';
-import UpdateProductPage from '../pages/products/UpdateProductPage';
-import ProductDetailPage from '../pages/products/ProductDetailPage';
-import HomePage from '../pages/home/HomePage';
-import AboutPage from '../pages/about/AboutPage';
+
+const HomePage = lazy(() => import('../pages/home/HomePage'));
+const LoginPage = lazy(() => import('../pages/login/LoginPage'));
+const AboutPage = lazy(() => import('../pages/about/AboutPage'));
+const ProductListPage = lazy(() => import('../pages/products/ProductListPage'));
+const AddProductPage = lazy(() => import('../pages/products/AddProductPage'));
+const UpdateProductPage = lazy(
+	() => import('../pages/products/UpdateProductPage'),
+);
+const ProductDetailPage = lazy(
+	() => import('../pages/products/ProductDetailPage'),
+);
 
 export const router = createBrowserRouter([
-	{ path: '/', element: <HomePage /> },
-	{ path: '/login', element: <LoginPage /> },
-	{ path: '/about', element: <AboutPage /> },
-	{ path: '/products', element: <ProductListPage /> },
-	{ path: '/add-product', element: <AddProductPage /> },
-	{ path: '/update-product/:id', element: <UpdateProductPage /> },
-	{ path: '/detail-product/:id', element: <ProductDetailPage /> },
+	{
+		path: '/',
+		element: (
+			<Suspense fallback={<LoadingComponent />}>
+				<HomePage />
+			</Suspense>
+		),
+	},
+	{
+		path: '/login',
+		element: (
+			<Suspense fallback={<LoadingComponent />}>
+				<LoginPage />
+			</Suspense>
+		),
+	},
+	{
+		path: '/about',
+		element: (
+			<Suspense fallback={<LoadingComponent />}>
+				<AboutPage />
+			</Suspense>
+		),
+	},
+	{
+		path: '/products',
+		element: (
+			<Suspense fallback={<LoadingComponent />}>
+				<ProductListPage />
+			</Suspense>
+		),
+	},
+	{
+		path: '/add-product',
+		element: (
+			<Suspense fallback={<LoadingComponent />}>
+				<AddProductPage />
+			</Suspense>
+		),
+	},
+	{
+		path: '/update-product/:id',
+		element: (
+			<Suspense fallback={<LoadingComponent />}>
+				<UpdateProductPage />
+			</Suspense>
+		),
+	},
+	{
+		path: '/detail-product/:id',
+		element: (
+			<Suspense fallback={<LoadingComponent />}>
+				<ProductDetailPage />
+			</Suspense>
+		),
+	},
 	{ path: '*', element: <Error404 /> },
 ]);
