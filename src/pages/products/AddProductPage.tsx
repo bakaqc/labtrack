440e59661e231c9cf1d07473a65291032b176960
Minh Vương <hardingadonis@gmail.com>
2024-03-20 08:46:25 +0700

    update navbar

--- src/pages/products/AddProductPage.tsx
+++ src/pages/products/AddProductPage.tsx
@@ -1,5 +1,19 @@
+import { Suspense, lazy } from 'react';
+
+const NavBarComponent = lazy(
+	() => import('../../components/common/NavbarComponent'),
+);
+
 const AddProductPage = () => {
-	return <></>;
+	return (
+		<>
+			<div className="vh-100">
+				<Suspense fallback={<div>Loading...</div>}>
+					<NavBarComponent />
+				</Suspense>
+			</div>
+		</>
+	);
 };
 
 export default AddProductPage;
