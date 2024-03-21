ed851ad766e706f545438c9fead6b35c09b94c6c
Yuht <nguyenthithuy1022003@gmail.com>
2024-03-21 15:58:27 +0700

    ProductDetail(Completed)

--- src/components/product/ProductDetailForm.tsx
+++ src/components/product/ProductDetailForm.tsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SERVER_URL } from '../../api/config';
+
+import { useNavigate } from 'react-router-dom';
+
 
 interface ProductDetailFormProps {
   id: number;
@@ -8,6 +11,7 @@ interface ProductDetailFormProps {
 
 const ProductDetailForm = (props: ProductDetailFormProps) => {
   const [product, setProduct] = useState<ProductInfo | null>(null);
+	const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -23,9 +27,17 @@ const ProductDetailForm = (props: ProductDetailFormProps) => {
     return <div>Loading...</div>;
   }
 
+	const handleBackClick = () => {
+    navigate('/products');
+  }
+
 	const formatPrice = (price: number) => {
 		return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.') + 'đ';
 	};
+	
+	const calculateDiscount = (price: number, currentPrice: number) => {
+    return ((1 - (currentPrice / price)) * 100).toFixed(0);
+  };
 
 	return (
 		<div className="container mt-5 w-50 shadow-lg py-5">
@@ -35,9 +47,12 @@ const ProductDetailForm = (props: ProductDetailFormProps) => {
 				</div>
 				<div className="col-6">
 					<h2 className="mb-4 text-start">{product.name}</h2>
-					<h6 className="d-flex flex-column align-items-center mb-3">Giá tiền: {formatPrice(product.price)}</h6>
-					<h6 className="d-flex flex-column align-items-center mb-3">Ưu đãi: {formatPrice(product.currentPrice)}</h6>
+					<h6 className="mb-3" style={{ whiteSpace: 'nowrap' }}>
+						Giá tiền: <span style={{ textDecoration: product.currentPrice < product.price ? 'line-through' : 'none' }}>{formatPrice(product.price)}</span></h6>
+					<h6 className="whiteSpace: 'nowrap'" style={{ color: 'red' }}>
+						Ưu đãi (-{calculateDiscount(product.price, product.currentPrice)}%): {formatPrice(product.currentPrice)} </h6>
 					<h6 className="d-flex flex-column align-items-center mb-3">Mô tả sản phẩm{product.description}</h6>
+				<button onClick={handleBackClick} className="btn btn-primary float-right">Quay lại</button>
 				</div>
 			</div>
 		</div>
